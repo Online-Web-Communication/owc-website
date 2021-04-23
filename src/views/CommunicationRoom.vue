f5eeb0f886dc987febfe1238356b6ce130970813
Sefa <60031458+fesa6654@users.noreply.github.com>
2021-04-23 22:27:40 +0300

    Initial commit

---
+++ src/views/CommunicationRoom.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container-fluid">
+    <b-button
+      class="back-button"
+      size="sm"
+      variant="primary"
+      @click="backToLogin()"
+      ><b-icon class="mr-2" icon="arrow-left-circle"></b-icon>Çıkış</b-button
+    >
+    <div class="row justify-content-start align-items-center">
+      <div class="col-md-4" v-for="data in 8" :key="data">
+        <video id="localVideo" autoplay="autoplay" controls>
+          <source :src="link" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+    <video class="personalScreen" id="localVideoa" autoplay="autoplay">
+      <source
+        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/h264.mov"
+        type="video/mp4"
+      />
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/h264.mov",
+    };
+  },
+  methods: {
+    backToLogin() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#localVideo {
+  width: 100% !important;
+  height: 350px !important;
+}
+
+.col-md-4 {
+  padding: 0 0 !important;
+}
+
+.back-button {
+  position: fixed !important;
+  top: 10px !important;
+  left: 10px !important;
+  opacity: 0.5 !important;
+  z-index: 1000 !important;
+}
+
+.back-button:hover {
+  opacity: 1 !important;
+}
+
+video {
+  width: 100% !important;
+  height: 99vh !important;
+}
+
+.personalScreen {
+  position: fixed !important;
+  bottom: 10px !important;
+  right: 10px !important;
+  width: 225px !important;
+  height: 150px !important;
+  border-radius: 10px !important;
+  z-index: 10 !important;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container-fluid">
+    <div style="display: grid">
+      <div
+        class="row justify-content-center align-items-center text-center mt-5"
+      >
+        <div class="col">
+          <h2>Online Web Communication</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center mt-5">
+        <div class="col">
+          <b-card>
+            <label for="input1" class="form-label"
+              >Katılmak İstediğiniz Odanın Adını Yazınız</label
+            >
+            <input
+              v-model="roomName"
+              type="text"
+              class="form-control"
+              id="input1"
+            />
+            <b-button class="mt-3" variant="success" @click="login()"
+              ><b-icon class="mr-2" icon="box-arrow-in-right"></b-icon
+              >Giriş</b-button
+            >
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      roomName: "",
+    };
+  },
+  methods: {
+    login() {
+      if (this.roomName !== "") {
+        this.$router.push({
+          name: "CommunicationRoom",
+          params: { room: this.roomName },
+        });
+      } else {
+        this.$bvToast.toast("Oda İsmi Yazınız !", {
+          title: "Giriş Yapılamadı !",
+          variant: "danger",
+          solid: true,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container-fluid {
+  width: 100% !important;
+  height: 70vh !important;
+  display: flex !important;
+  justify-content: center !important;
+  align-items: center !important;
+}
+
+.form-label {
+  font-size: 14px !important;
+}
+
+.form-control {
+  width: 260px !important;
+}
+</style>
